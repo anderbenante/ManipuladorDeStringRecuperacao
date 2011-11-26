053fea09471cc932ef9b8d279085a0c4b88844c0
anderbenante <anderson.benante@gmail.com>
2011-11-26 20:39:38 -0200

    atualizacao_tres

--- test/ManipuladorDeStringRecuperacaoTest.java
+++ test/ManipuladorDeStringRecuperacaoTest.java
@@ -7,31 +7,31 @@ public class ManipuladorDeStringRecuperacaoTest {
 
 	@Test
 	public void deveOrdenarFraseRatoRoeu() {
-		String fraseOrdenNormal = ManipuladorDeString.ordenar("O rato roeu a roupa do rei de Roma");
-		assertEquals("O a do de rei rato roeu Roma roupa", fraseOrdenNormal);
+		String fraseCrescente = ManipuladorDeString.ordenar("O rato roeu a roupa do rei de Roma");
+		assertEquals("O a do de rei rato roeu Roma roupa", fraseCrescente);
 	}
 	
 	@Test
 	public void deveOrdenarFraseJabuti() {
 		String fraseCrescente = ManipuladorDeString.ordenar("Um pequeno jabuti xereta viu dez cegonhas felizes");
-		assertEquals("Um viu dez jabuti xereta pequeno felizes cegonhas",fraseCrescente );
+		assertEquals("Um viu dez jabuti xereta pequeno felizes cegonhas",fraseCrescente);
 	}
 	
 	@Test
 	public void deveOrdenarFraseHareKrishna() {
 		String fraseCrescente = ManipuladorDeString.ordenar("Cante Hare Krishna e seja Feliz");
-		assertEquals("e Hare seja Cante Feliz Krishna",fraseCrescente );
+		assertEquals("e Hare seja Cante Feliz Krishna",fraseCrescente);
 	}
 	
 	@Test
 	public void deveOrdenarFraseGrupo() {
 		String fraseCrescente = ManipuladorDeString.ordenar("Anderson Jackson e Marcelo");
-		assertEquals("e Jackson Marcelo Anderson",fraseCrescente );
+		assertEquals("e Jackson Marcelo Anderson",fraseCrescente);
 	}
 	
 	@Test
 	public void deveOrdenarFraseTempo() {
 		String fraseCrescente = ManipuladorDeString.ordenar("Preciso de mais tempo para estudar");
-		assertEquals("de mais para tempo Preciso estudar",fraseCrescente );
+		assertEquals("de mais para tempo Preciso estudar",fraseCrescente);
 	}	
 }
